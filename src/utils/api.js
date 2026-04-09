3296a7617e082860f30ef34413f2b0f369fd15f9
Prakriti Jha <prakritijha29aug@gmail.com>
2026-04-09 18:10:37 +0530

    Api calls for fetching repository details

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,3 +13,18 @@ export const searchUsers = async (query) => {
   const response = await githubApi.get(`/search/users?q=${query}`)
   return response.data.items
 }
+
+export const getUserProfile = async (username) => {
+  const response = await githubApi.get(`/users/${username}`)
+  return response.data
+}
+
+export const getUserRepos = async (username) => {
+  const response = await githubApi.get(`/users/${username}/repos?per_page=100`)
+  return response.data
+}
+
+export const getRepoDetails = async (username, reponame) => {
+  const response = await githubApi.get(`/repos/${username}/${reponame}`)
+  return response.data
+}
